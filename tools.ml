a5c45bcb983168f59f3d3ecc2990ae0c984ea662
Luis97Lopez <35906037+Luis97Lopez@users.noreply.github.com>
2021-11-15 18:09:37 +0100

    Simplification clone nodes

--- tools.ml
+++ tools.ml
@@ -1,6 +1,6 @@
 open Graph
 
-let clone_nodes (graph:'a graph) = n_fold graph (fun acc id -> new_node acc id) empty_graph
+let clone_nodes (graph:'a graph) = n_fold graph new_node empty_graph
 
 let gmap (graph:'a graph) f = e_fold graph (fun acc id1 id2 label -> new_arc acc id1 id2 (f label)) (clone_nodes graph)
 
