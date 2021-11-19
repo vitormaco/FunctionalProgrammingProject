170b2f426ab908aa8d884e98d875cbba764b75d8
Luis97Lopez <35906037+Luis97Lopez@users.noreply.github.com>
2021-11-19 12:16:55 +0100

    Find path INCOMPLETE

--- tools.ml
+++ tools.ml
@@ -7,3 +7,18 @@ let gmap (graph:'a graph) f = e_fold graph (fun acc id1 id2 label -> new_arc acc
 let add_arc (graph: int graph) id1 id2 n = match (find_arc graph id1 id2) with 
     | None -> new_arc graph id1 id2 n
     | Some x-> new_arc graph id1 id2 (n+x)
+
+let rec find_path (graph: int graph) forbidden id1 id2 = 
+    let forbidden = (List.append forbidden [Some id1]) in 
+    match (find_arc graph id1 id2) with
+        | Some x -> (List.append forbidden [Some id2])
+        | None ->
+            let arcs = out_arcs graph id1 in
+            let rec loop = (function
+                | [] -> [];
+                | (a, b) :: rest -> 
+                    match (find_path graph forbidden b id2) with
+                    | 
+                    |        
+            ;) in
+            List.append forbidden (loop arcs)
