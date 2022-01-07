7365cacad936ebd97c70bace767aa7eb014ec2ab
Luis97Lopez <35906037+Luis97Lopez@users.noreply.github.com>
2022-01-07 13:55:37 +0100

    Print bonito

--- cfile.mli
+++ cfile.mli
@@ -1,8 +1,11 @@
 (* Read a company from a file *)
 open Company
+open Graph
 
 type path = string
 
 (* Values are read as strings. *)
 val from_cfile: path -> company
 
+(* export graph to graph viz format *)
+val export_file_graphviz_company: path -> string graph -> company -> int -> int -> unit
